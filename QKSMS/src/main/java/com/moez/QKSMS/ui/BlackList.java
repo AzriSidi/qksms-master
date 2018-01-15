f1ce725c3cb708b90e1e38b2b620b163d3668d46
Azri <indieazri@gmai.com>
2018-01-15 17:31:04 +0800

    Initial commit

---
+++ QKSMS/src/main/java/com/moez/QKSMS/ui/BlackList.java
@@ -0,0 +1,13 @@
+package com.moez.QKSMS.ui;
+
+public class BlackList {
+    private String BlackListNumber;
+
+    public String getBlackListNumber() {
+        return BlackListNumber;
+    }
+
+    public void setBlackListNumber(String blackListNumber) {
+        this.BlackListNumber = blackListNumber;
+    }
+}
